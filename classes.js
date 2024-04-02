58b8b2d3b82ab113e0346c427a3368c51011c633
GiDFerreira <giovannadferreira@alunos.utfpr.edu.br>
2024-04-02 16:16:50 -0300

    classes update
    adicionando Armazenamentos e Gerenciador de Contatos

--- classes.js
+++ classes.js
@@ -5,3 +5,43 @@ class Contato{
         this.email = email;
     }
 }
+
+class ArmazenamentoContatos {
+    constructor() {
+      this.contatos = [];
+    }
+  
+    adicionar(contato) {
+      this.contatos.push(contato);
+    }
+  
+    remover(contato) {
+      const index = this.contatos.indexOf(contato);
+      if (index !== -1) {
+        this.contatos.splice(index, 1);
+      }
+    }
+  
+    listar() {
+      return this.contatos;
+    }
+  }
+
+  class GerenciadorContatos {
+    constructor(adaptador) {
+      this.adaptador = adaptador;
+    }
+  
+    adicionarContato(nome, telefone, email) {
+      const contato = new Contato(nome, telefone, email);
+      this.adaptador.adicionar(contato);
+    }
+  
+    removerContato(contato) {
+      this.adaptador.remover(contato);
+    }
+  
+    listarContatos() {
+      return this.adaptador.listar();
+    }
+  }
